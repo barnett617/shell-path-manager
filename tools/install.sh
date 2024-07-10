6c04691191877332fc349f5dd5777d1de8f0ab50
Wilson <scorpioaoe@gmail.com>
2024-07-11 00:49:42 +0800

    feat: add download progress andrefactor the final installation operation

--- tools/install.sh
+++ tools/install.sh
@@ -23,14 +23,6 @@ else
   echo "Directory already exists: $SPM_HOME"
 fi
 
-# Create the record file inside .spm if it doesn't exist
-if [ ! -f "$SPM_RECORD" ]; then
-  touch "$SPM_RECORD"
-  echo "Created file: $SPM_RECORD"
-else
-  echo "File already exists: $SPM_RECORD"
-fi
-
 # Create the bin directory inside .spm if it doesn't exist
 if [ ! -d "$SPM_BIN" ]; then
   mkdir "$SPM_BIN"
@@ -39,49 +31,58 @@ else
   echo "Directory already exists: $SPM_BIN"
 fi
 
+# Create the record file inside .spm if it doesn't exist
+if [ ! -f "$SPM_RECORD" ]; then
+  touch "$SPM_RECORD"
+  echo "Created file: $SPM_RECORD"
+else
+  echo "File already exists: $SPM_RECORD"
+fi
+
 # Check if the file exist
 spm_has() {
-  type "$1" > /dev/null 2>&1
+  type "$1" >/dev/null 2>&1
 }
 
 # Download helper
 spm_download() {
   if spm_has "curl"; then
-    curl --fail --compressed -q "$@"
+    echo "Downloading binary file: $uri"
+    curl -# --fail --compressed -q "$@"
   fi
 }
 
+# Clear helper
 clear_spm() {
   rm -rf "$SPM_HOME"
 }
 
-# Download the binary file from github
-download_executable() {
-  local uri=$1
-  local executable=$2
+# Append spm bin directory to PATH
+append_path() {
+  # Switch to Zsh
+  chsh -s /bin/zsh
 
-  if spm_download -s $uri -o $executable; then
-    chmod +x $executable
-    echo "Downloaded binary file: $executable"
-  else
-    echo "Failed to download spm executable"
-    clear_spm
-  fi
-}
-
-download_executable $SPM_EXECUTABLE_URI $SPM_EXECUTABLE || {
-  { echo "Failed to set permission on spm executable"; exit 1; }
-}
-
-# Add the bin directory to PATH and update it immediately
-if [[ ":$PATH:" == *":$SPM_BIN:"* ]]; then
-  echo "The spm bin directory is already in PATH"
-else
-  echo "export PATH=\"$SPM_BIN:\$PATH\"" >> "$HOME/.zshrc"
+  # Add the bin directory to PATH
+  echo "export PATH=\"$SPM_BIN:\$PATH\"" >>"$HOME/.zshrc"
   if source "$HOME/.zshrc"; then
     echo "spm is ready to use"
   else
     echo "Failed to source .zshrc. Please manually add the following line to your .zshrc file:"
     echo "export PATH=\"$SPM_BIN:\$PATH\""
   fi
+}
+
+# Download the spm executable and set permission
+if spm_download $SPM_EXECUTABLE_URI $SPM_EXECUTABLE; then
+  if chmod +x $SPM_EXECUTABLE; then
+    append_path
+  else
+    echo "Failed to set permission on spm executable"
+    echo "Please manually add the following line to your .zshrc file:"
+    echo "export PATH=\"$SPM_BIN:\$PATH\""
+  fi
+else
+  echo "Failed to download spm executable"
+  clear_spm
+  exit 1
 fi
--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,1 +1,22 @@
-rm -rf ~/.spm
+# Remove the bin directory from PATH if exists
+function remove_spm_path() {
+  if [[ ":$PATH:" == *":$SPM_BIN:"* ]]; then
+    export PATH=$(echo $PATH | sed "s|:$SPM_BIN:|::" | sed "s|^:$||" | sed "s|:$||")
+    echo "Removed the spm bin directory from PATH"
+  else
+    echo "The spm bin directory is not in PATH"
+  fi
+}
+
+# Uninstall spm
+function uninstall() {
+  if rm -rf "$SPM_HOME"; then
+    echo "All spm related files are removed"
+  else
+    echo "Failed to remove $SPM_HOME"
+  fi
+
+  remove_spm_path
+}
+
+uninstall
