272c2c098edb18f4cfbc56002ae26b8d3e11cfd1
Wilson <scorpioaoe@gmail.com>
2024-07-15 23:56:16 +0800

    refactor: spm binary and uninstallation script

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,19 +1,9 @@
 SPM_HOME="$HOME/.spm"
 SPM_BIN="$SPM_HOME/bin"
 
-# Remove the bin directory from PATH if exists
-function remove_spm_path() {
-    export PATH=$(echo "${PATH}" | sed '|'"$SPM_BIN":||g' | sed '|:?'"$SPM_BIN"'||g')
-}
-# Uninstall spm
-function uninstall() {
-  if rm -rf "$SPM_HOME"; then
-    echo "All spm related files are removed"
-  else
-    echo "Failed to remove $SPM_HOME"
-  fi
-
-  remove_spm_path
-}
-
-uninstall
+echo "Removing ~/.spm"
+if [ -d "$SPM_HOME" ]; then
+  rm -rf "$SPM_HOME"
+else
+  echo "The spm home directory does not exist"
+fi
